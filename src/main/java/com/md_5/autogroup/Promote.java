febd793601c1df4b887c74fd0f5f0c714f900927
md-5 <md_5@bigpond.com>
2011-11-19 18:10:12 +1100

    No rank

--- src/main/java/com/md_5/autogroup/Promote.java
+++ src/main/java/com/md_5/autogroup/Promote.java
@@ -37,6 +37,9 @@ public class Promote {
     }
 
     public static void promotePlayer(String player, String group) {
+        if (Bukkit.getServer().getPlayer(player).hasPermission("autogroup.norank")){
+            return;
+        }
         AutoGroup.logger.info(String.format("AutoGroup: Trying to promote %1$s to group " + group, player));
         for (Player p : Bukkit.getServer().getOnlinePlayers()) {
             p.sendMessage(ChatColor.LIGHT_PURPLE + "AutoGroup: Please welcome " + player + " to group " + group);
